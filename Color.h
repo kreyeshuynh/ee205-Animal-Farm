bf40e03dad1cd4dd38b38029ceb7d59fda7fc8ba
tacos <kaianu@hawaii.edu>
2022-04-24 14:00:13 -1000

    implemented Color.h

--- Color.h
+++ Color.h
@@ -8,8 +8,66 @@
 /// @author Kaianu Reyes-Huynh <@kaianu@hawaii.edu>
 /// @date   24_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
+#pragma once
 
-#ifndef EE205_ANIMAL_FARM_COLOR_H
-#define EE205_ANIMAL_FARM_COLOR_H
+#include <iostream>
+#include <string>
 
-#endif //EE205_ANIMAL_FARM_COLOR_H
+#include "config.h"
+
+enum class Color {
+    UNKNOWN_COLOR=0
+    ,BLACK
+    ,BROWN
+    ,WHITE
+    ,RED
+    ,BLUE
+    ,GREEN
+    ,GINGER
+    ,CREAM
+    ,CINNAMON
+    ,CALICO
+} ;
+
+inline std::ostream& operator<<( std::ostream& lhs_stream, const Color& rhs_Color ) {
+    switch( rhs_Color ) {
+        case Color::UNKNOWN_COLOR:
+            lhs_stream << "Unknown color";
+            break;
+        case Color::BLACK:
+            lhs_stream << "Black";
+            break;
+        case Color::BROWN:
+            lhs_stream << "Brown";
+            break;
+        case Color::WHITE:
+            lhs_stream << "White";
+            break;
+        case Color::RED:
+            lhs_stream << "Red";
+            break;
+        case Color::BLUE:
+            lhs_stream << "Blue";
+            break;
+        case Color::GREEN:
+            lhs_stream << "Green";
+            break;
+        case Color::GINGER:
+            lhs_stream << "Ginger";
+            break;
+        case Color::CREAM:
+            lhs_stream << "Cream";
+            break;
+        case Color::CINNAMON:
+            lhs_stream << "Cinnamon";
+            break;
+        case Color::CALICO:
+            lhs_stream << "Calico";
+            break;
+        default:
+            //Color not mapped to string
+            throw std::out_of_range( PROGRAM_NAME ": Color not mapped to a string" );
+    }
+
+    return lhs_stream;
+}
